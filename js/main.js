fa8cbd70821ceae012fcd6d0e0b5ae914b55931c
Leonid <leonid.domagalalsky@gmail.com>
2024-05-30 21:20:44 +0200

    update

--- js/main.js
+++ js/main.js
@@ -65,3 +65,27 @@ document.addEventListener("DOMContentLoaded", function () {
     lastScrollTop = scrollTop;
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".timeline__item");
+  const navLi = document.querySelectorAll(".timeline__sidenav a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLi.forEach((li) => {
+      li.classList.remove("active");
+      if (li.getAttribute("href").includes(current)) {
+        li.classList.add("active");
+      }
+    });
+  });
+});

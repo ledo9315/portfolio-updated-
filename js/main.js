b35fc6a67d5d7e0010075f820e078c3b22d48b16
Leonid <leonid.domagalalsky@gmail.com>
2024-10-18 22:19:53 +0200

    portfolio

--- js/main.js
+++ js/main.js
@@ -2,53 +2,13 @@ import { initHamburgerMenu } from "./hamburger.js";
 import { initNavigation, initScrollNavigation } from "./navigation.js";
 import { initSectionHighlighting } from "./timeline.js";
 import { initializeLanguageSwitcher, changeLanguage } from "./lang.js";
+import { Toast } from "./toast.js";
 
 initHamburgerMenu();
 initNavigation();
 initScrollNavigation();
 initSectionHighlighting();
 initializeLanguageSwitcher();
+new Toast();
 
 window.changeLanguage = changeLanguage;
-
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", async function (e) {
-    e.preventDefault(); // Verhindere das Standardverhalten (kein Reload, kein Redirect)
-
-    const form = e.target;
-    const formData = new FormData(form);
-
-    try {
-      const response = await fetch(form.action, {
-        method: "POST",
-        body: formData,
-      });
-
-      if (response.ok) {
-        // Zeige den Toast bei erfolgreichem Absenden
-        showToast();
-
-        // Formular zurücksetzen
-        form.reset();
-      } else {
-        // Fehlermeldung anzeigen, falls etwas schiefgeht
-        alert("Ein Fehler ist aufgetreten. Bitte versuche es erneut.");
-      }
-    } catch (error) {
-      console.error("Fehler:", error);
-      alert(
-        "Es ist ein Netzwerkfehler aufgetreten. Bitte versuche es später erneut."
-      );
-    }
-  });
-
-function showToast() {
-  const toast = document.getElementById("toast");
-  toast.style.display = "block"; // Zeige den Toast an
-  toast.classList.add("bounceInRight");
-
-  setTimeout(() => {
-    toast.style.display = "none"; // Blende den Toast nach 3 Sekunden aus
-  }, 3000);
-}
